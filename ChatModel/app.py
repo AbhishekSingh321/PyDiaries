1d3b2994df893a5b58f1f7fb262c40b82e4d3ba6
AbhishekSingh321 <singhabhi292004@gmail.com>
2025-11-05 16:06:49 +0530

    ChatModel stopping condition

--- ChatModel/app.py
+++ ChatModel/app.py
@@ -1,9 +1,13 @@
 import chatModel
 
+print('=================================================================================')
+print('Say the chatbot for stop or enter exit')
+print('=================================================================================')
+
 while True:
   query=input('Ask something! : ')
-  if(query=='exit'):
+  response=chatModel.ask(query,temperature=0.8 )     #0.8 more creative                     
+  if(response=='exit'):
     break
   else:
-    response=chatModel.ask(query,temperature=0.8 )     #0.8 more creative
-    print(response)  
+    print(response)
--- ChatModel/chatModel.py
+++ ChatModel/chatModel.py
@@ -7,7 +7,8 @@ load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def ask(query,model='llama-3.1-8b-instant',temperature=0.2):
-   sysPrompt='You are friendly AI expert solve the user query in a concise way'
+   sysPrompt='''1) You are friendly AI expert solve the user query in a concise way
+                2) If user want to exit or say for stop then only genearte 'exit' keyword and nothing extra'''
    chat_completion = client.chat.completions.create(
     messages=[
        {"role": "system","content": sysPrompt},
