48be9601eda5ffeb21c311004f86c25f3263317e
AbhishekSingh321 <singhabhi292004@gmail.com>
2025-11-05 14:58:51 +0530

    using dynamic query for chatModel

--- ChatModel/app.py
+++ ChatModel/app.py
@@ -1,21 +1,5 @@
-import os
-from dotenv import load_dotenv
-from groq import Groq
+import chatModel
 
-load_dotenv()
+query='What is Gravity'
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
-
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Explain the Gravitational Force",
-        }
-    ],
-    model="llama-3.1-8b-instant",
-)
-
-print(chat_completion.choices[0].message.content)
+chatModel.ask(query)
---
+++ ChatModel/chatModel.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+def ask(query):
+   chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": query,
+        }
+    ],
+    model="llama-3.1-8b-instant",
+    )
+
+   print(chat_completion.choices[0].message.content)
